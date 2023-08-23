348348c531d1daf8b428f172085008ba088df94f
OsamaMansour25 <osam0289@stud.kea.dk>
2023-08-23 20:13:28 +0200

    Initial commit

---
+++ src/main/java/com/example/day1/API/PlayerController.java
@@ -0,0 +1,27 @@
+package API;
+
+import Repositories.PlayerRepository;
+import com.example.day1.Entity.Player;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/players")
+public class PlayerController {
+
+    PlayerRepository playerRepository;
+
+    public PlayerController(PlayerRepository playerRepository) {
+        this.playerRepository = playerRepository;
+    }
+
+    @GetMapping
+    List<Player> getPlayers() {
+        return  playerRepository.findAll();
+    }
+}
+
+
